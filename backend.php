0a4ea789fd75cdd62ff11765e35256d7c73ebf01
Aayush sah <scammery51@gmail.com>
2026-05-27 22:12:30 +0545

    initial

---
+++ backend.php
@@ -0,0 +1,7 @@
+<?php
+
+$name=$_GET["name"];
+$password=$_GET["password"];
+echo $name;
+echo "<br/>";
+echo $password;
---
+++ from.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="backend.php" method="get">
+        <label for="Name">
+        Name: <input type="text" placeholder="please enter your name" name="name">
+        </label>
+        <label for="Password">
+        Password: <input type="text" placeholder="please enter your password" name="password">
+        </label>
+           <input type="submit">
+    </form>
+</body>
+</html>
